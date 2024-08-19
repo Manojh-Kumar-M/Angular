57eb5cd87d17ba771e07ab98a7b52e9afb4364b9
Manojh kumar <manojhkumarjes@gmail.com>
2024-08-19 10:03:20 +0530

    style and class binding in angular 18

--- basic_angular-sample/sample1/interpolation/src/app/test/test.component.ts
+++ basic_angular-sample/sample1/interpolation/src/app/test/test.component.ts
@@ -4,11 +4,40 @@ import { Component } from '@angular/core';
   selector: 'app-test',
   standalone: true,
   imports: [],
-  template: `<h2>
-    Welcomes India
-  </h2>`,
-  styleUrl: './test.component.scss'
+  template: `
+  <h1 [style.color]="haserror?'red':'green'">style binding</h1>
+  <input  #myinput bind-disabled="did" [id]="myID" type="text" value="name"/>
+  <Button (click)="onclick($event)">Greet</Button>
+  {{this.greeting}}`,
+  styles: [`
+    .text-success{
+      color:green;
+    }
+    .text-danger{
+      color:red;
+    }
+    .text-special{
+      font-style:Bold;
+    }`]
 })
 export class TestComponent {
-
+name ="vishwas";
+myID="VishwaParama";
+successclass= "text-success";
+public did=false;
+public haserror= false;
+public textSpecial= true;
+public greeting="";
+public messageClass={
+  "text-success":!this.successclass,
+  "text-danger":!this.haserror,
+  "text-special":this.textSpecial
+}
+greet(){
+  return "hello "+ this.greeting;
+}
+onclick(Event:any){
+console.log(this.greet());
+this.greeting=Event.type;
+}
 }
